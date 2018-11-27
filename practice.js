3569e80629e9b9c23c017afd3b660c591a9064b6
Hans Thummel <enikath@gmail.com>
2018-11-27 15:17:25 -0600

    first pass

--- practice.js
+++ practice.js
@@ -14,9 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+var me = {
+  name: "John Thummel",
+  age: 27
+};
 
-
+alert(me.name);
 
 ////////// PROBLEM 2 //////////
 
@@ -25,25 +28,28 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
-
-
+var favoriteThings = {
+  band: "Reol",
+  food: "Thai Curry",
+  person: "Keanu Reeves",
+  book: "Sirens of Titan",
+  movie: "Cloud Atlas",
+  holiday: "Halloween"
+};
 
 /*
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
-
-
+favoriteThings.car = "'75 Lincoln Contenental";
+favoriteThings.brand = "Cattle Brand";
 
 /*
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
-
-
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 ////////// PROBLEM 3 //////////
 
@@ -55,17 +61,15 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
-
-
-
+var backPack = {};
+var item = "firstPocket";
+backPack[item] = "chapstick";
+backPack.color = "black";
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
-
-
+alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -73,20 +77,18 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
-
-
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
 var user2 = {
-  name: 'Ty',
+  name: "Ty",
   age: 24,
-  pwHash: 'U+Ldlngx2BYQk',
-  email: 'ty33@gmail.com',
-  birthday: '05/02/1990',
-  username: 'tylermcginnis33'
+  pwHash: "U+Ldlngx2BYQk",
+  email: "ty33@gmail.com",
+  birthday: "05/02/1990",
+  username: "tylermcginnis33"
 };
 // Do not edit the code above.
 
@@ -96,22 +98,11 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
-
-
-
-
-
-
+user2.name = "Tyler S. McGinnis";
+user2.email = "tyler.mcginnis@devmounta.in";
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
-
-
-
-
-
-
 
 ////////// PROBLEM 5 //////////
 
@@ -121,8 +112,6 @@ var user2 = {
 
 //Code Here
 
-
-
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object. 
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console. 
@@ -130,15 +119,11 @@ var user2 = {
 
 //Code Here
 
-
-
 /*
   Now call your alertHello and logHello methods.
 */
 
 //Code Here
-
-
 
 ////////// PROBLEM 6 //////////
 
@@ -149,8 +134,6 @@ var user2 = {
 
 //Code Here
 
-
-
 ////////// PROBLEM 7 //////////
 
 /*
@@ -159,5 +142,3 @@ var user2 = {
 */
 
 //Code Here
-
-
